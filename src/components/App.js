7ad50d1cc9374277a11c4d722d3fffdb5326a76d
Marc Newport <marcnewport@gmail.com>
2019-02-01 16:58:25 +1100

    change data structure

--- src/components/App.js
+++ src/components/App.js
@@ -5,31 +5,7 @@ import Column from './Column';
 import styled from 'styled-components';
 import '@atlaskit/css-reset';
 
-import {data, mapData} from '../data';
-
-import qs from 'qs';
-
-const url = 'https://api.productive.io/api/v2/tasks';
-const now = Date.now();
-// const after = now - (1.21e+9);
-const before = now;
-const status = 1;
-const page = {
-  size: 200,
-};
-
-const query = qs.stringify({
-  // after,
-  before,
-  status,
-  page,
-});
-
-const headers = {
-  'X-Auth-Token': '3a403a0e-f76e-4204-b26e-e3a014455e0d',
-  'X-Organization-Id': '2555',
-  'Content-Type': 'application/vnd.api+json; charset=utf-8',
-};
+import {data, getData} from '../data';
 
 const Container = styled.div`
   display: flex;
@@ -39,11 +15,8 @@ const Container = styled.div`
 class App extends Component {
   state = data;
 
-  componentDidMount() {
-    fetch(url + '?' + query, {headers})
-      .then(response => response.json())
-      .then(response => this.setState(mapData(response)))
-      .catch(error => console.error(error));
+  async componentDidMount() {
+    this.setState(await getData());
   }
 
   render() {
--- src/data.js
+++ src/data.js
@@ -1,3 +1,138 @@
+import qs from 'qs';
+
+const url = 'https://api.productive.io/api/v2/';
+
+const headers = {
+  'X-Auth-Token': '3a403a0e-f76e-4204-b26e-e3a014455e0d',
+  'X-Organization-Id': '2555',
+  'Content-Type': 'application/vnd.api+json; charset=utf-8',
+};
+
+
+/**
+ * [getBoards description]
+ * @return {[type]} [description]
+ */
+async function getDevelopmentBoards() {
+
+  const boards = await fetch(url + 'boards', {headers})
+    .then(response => response.json())
+    .then(response => response.data.filter(board => board.attributes.name === 'Development'))
+    .catch(error => console.error(error));
+
+  console.log(boards);
+
+  return boards.map(board => board.id);
+}
+
+
+/**
+ * [getTasks description]
+ * @return {[type]} [description]
+ */
+async function getTaskData() {
+  const now = Date.now();
+  const after = now - (1.21e+9);
+  const before = now;
+
+  const filter = {
+    status: 1,
+  };
+
+  const page = {
+    size: 200,
+  };
+
+  const query = qs.stringify({
+    // after,
+    // before,
+    filter,
+    page,
+  });
+
+  return await fetch(url + 'tasks?' + query, {headers})
+    .then(response => response.json())
+    .then(response => response)
+    .catch(error => console.error(error));
+}
+
+
+/**
+ * [mapData description]
+ * @param  {[type]} response [description]
+ * @return {[type]}          [description]
+ */
+export async function getData() {
+
+  const developmentBoards = await getDevelopmentBoards();
+
+  console.log('developmentBoards', developmentBoards);
+
+  const taskData = await getTaskData();
+
+  console.log('taskData', taskData);
+
+  // The allowed columns for this sprint
+  const allowed = [
+    'to do',
+    'in progress',
+    'blocked',
+    'with client',
+    'ready to deploy',
+    'completed',
+  ];
+
+  const backgrounds = [
+    '#fafafa',
+    '#fafaff',
+    '#fffafa',
+    'white',
+    'white',
+    '#fafffa'
+  ];
+
+  // Map the allowed columns into a more usable structure
+  let columns = allowed.map((title, id) => {
+    return {
+      id,
+      title,
+      tasks: [],
+      color: backgrounds[id],
+    };
+  });
+
+  // Look over each task and push it to the appropriate column
+  taskData.data.forEach((task, index) => {
+    // Map the task list to the task
+    let taskList = taskData.included.find(item => item.id === task.relationships.task_list.data.id);
+
+    // Does this tasklist belong to a development board?
+    if (developmentBoards.indexOf(taskList.relationships.board.data.id) === -1) {
+      return;
+    }
+
+    console.log('taskList', taskList);
+
+    // Check if the task list name is in the allowed columns
+    let column = columns.find(column => column.title === taskList.attributes.name.toLowerCase());
+
+    console.log(task);
+
+    // Push the task into the column
+    if (column) {
+      task.attributes.tag_list.forEach(tag => {
+        if (!isNaN(tag)) task.points = Number(tag);
+      });
+      column.tasks.push(task);
+    }
+  });
+
+  return {
+    columns,
+  }
+}
+
+
 // Dummy data
 export const data = {
   tasks: {
@@ -49,61 +184,3 @@ export const data = {
     }
   ],
 };
-
-export function mapData(response) {
-
-  console.log(response);
-
-  // The allowed columns for this sprint
-  const allowed = [
-    'to do',
-    'not started',
-    'in progress',
-    'blocked',
-    'testing',
-    'with client',
-    'completed',
-  ];
-
-  const backgrounds = [
-    'white',
-    '#fafafa',
-    '#fafaff',
-    '#fffafa',
-    'white',
-    'white',
-    '#fafffa'
-  ];
-
-  // Map the allowed columns into a more usable structure
-  let columns = allowed.map((title, id) => {
-    return {
-      id,
-      title,
-      tasks:[],
-      color: backgrounds[id],
-    };
-  });
-
-  // Look over each task and push it to the appropriate column
-  response.data.forEach((task, index) => {
-    // Map the task list to the task
-    let taskList = response.included.find(item => item.id === task.relationships.task_list.data.id);
-
-    // Check if the task list name is in the allowed columns
-    let column = columns.find(column => column.title === taskList.attributes.name.toLowerCase());
-    // Push the task into the column
-    if (column) {
-      task.attributes.tag_list.forEach(tag => {
-        if (!isNaN(tag)) task.points = Number(tag);
-      });
-      column.tasks.push(task);
-    }
-  });
-
-  console.log({columns});
-
-  return {
-    columns,
-  }
-}
